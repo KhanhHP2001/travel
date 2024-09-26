14a17847c3e01aa5deff82f5fb9d0ccf2ed46d51
Hoang Phi Khanh <phikhanh0169@gmail.com>
2024-09-26 09:06:22 +0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+
+import MobileNav from '@/components/Navbar/MobileNav';
+import Navbar from '@/components/Navbar/Navbar';
+
+import React, { useState } from 'react'
+
+const Homepage = () => {
+  const [nav, setNav] = useState(false);
+  const openNavHandler = () => setNav(true);
+  const closeNavHandler = () => setNav(false);
+  return (
+    <div className='overflow-x-hidden'>
+      {/* Navbar */}
+      <MobileNav nav={nav} closeNav={closeNavHandler} />
+      <Navbar openNav={openNavHandler} />
+    </div>
+  );
+}
+export default Homepage
